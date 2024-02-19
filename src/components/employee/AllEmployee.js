fb1a8fe690575621e5e1769477c220d1e0efff25
owaisezarger <owaisezarger@gmail.com>
2024-02-19 18:57:46 +0530

    add access column in all employee table

--- src/components/employee/AllEmployee.js
+++ src/components/employee/AllEmployee.js
@@ -20,9 +20,24 @@ export default function AllEmployee() {
   const [isDeleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
 
   const data = [
-    { email: "example1@example.com", username: "user1", action: "edit/delete" },
-    { email: "example2@example.com", username: "user2", action: "edit/delete" },
-    { email: "example3@example.com", username: "user3", action: "edit/delete" },
+    {
+      email: "example1@example.com",
+      username: "user1",
+      action: "edit/delete",
+      access: "block/unblock",
+    },
+    {
+      email: "example2@example.com",
+      username: "user2",
+      action: "edit/delete",
+      access: "block/unblock",
+    },
+    {
+      email: "example3@example.com",
+      username: "user3",
+      action: "edit/delete",
+      access: "block/unblock",
+    },
   ];
 
   const columns = useMemo(
@@ -84,6 +99,33 @@ export default function AllEmployee() {
                       d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                     />
                   </svg>
+                </button>
+              </div>
+            </div>
+          );
+        },
+      },
+      {
+        accessorKey: "access",
+        header: "Access",
+        size: 150,
+        Cell: ({ row }) => {
+          return (
+            <div className="text-xs">
+              <div className="flex items-center gap-4">
+                <button
+                  className="px-3 py-1 text-xs text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                  type="button"
+                  // onClick={() => handleEmployeeEdit(row)}
+                >
+                  Unblock
+                </button>
+                <button
+                  className="px-3 py-1 text-xs text-white bg-red-500 rounded hover:bg-red-800 focus:outline-none focus:bg-red-800"
+                  type="button"
+                  // onClick={() => handleEmployeeDelete(row)}
+                >
+                  Block
                 </button>
               </div>
             </div>
@@ -180,7 +222,7 @@ export default function AllEmployee() {
 
   return (
     <>
-      <h2 className="text-2xl font-bold">ITEM LIST</h2>
+      <h2 className="text-2xl font-bold">EMPLOYEE LIST</h2>
       <CommonTable columns={columns} data={data} />
       <Dialog
         open={isUpdateModalOpen}
